d817b6e30b80caac2509840f8aac7f84585fcedd
Haider Raza <88819339+SyedHaadi@users.noreply.github.com>
2024-12-09 11:36:45 +0000

    ok

---
+++ ios/FocusIos/PacketTunnelProvider.h
@@ -0,0 +1,12 @@
+//
+//  PacketTunnelProvider.h
+//  FocusIos
+//
+//  Created by ERS TECH on 15/02/2024.
+//
+
+#import <NetworkExtension/NetworkExtension.h>
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
